61ea44144cb1af8e9ab97ba625f73b55c408f1a7
German M <german.moreira@fing.edu.uy>
2020-08-12 23:31:45 -0300

    productos y login

--- js/products.js
+++ js/products.js
@@ -1,6 +1,48 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var currentProductsArray = [];
+
+function showProductsList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let category = currentProductsArray[i];
+
+/*         if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){ */
+
+            htmlContentToAppend += `
+            <a  class="list-group-item list-group-item-action" style="margin-left: auto; margin-right: auto; position: relative;">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                    </div>
+                    
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ category.name +`</h4>
+                            <small class="text-muted">` + category.soldCount + ` vendidos</small>
+                        </div>
+                        <p class="mb-1">` + category.description + `</p>
+                        <p class="mb-1">` + category.cost + " " + category.currency + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            currentProductsArray = resultObj.data;
+
+            showProductsList();
+        }
+    });
+
 
 });
